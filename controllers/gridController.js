eccce374c152291f3fb86837f7deca8dc90340b4
Félix Darraud <felix.darraud@gmail.com>
2020-02-16 12:02:01 +0100

    Added stringify methods + Re-think the boxBridge to prevent a bug on boxes' indexes

--- controllers/gridController.js
+++ controllers/gridController.js
@@ -1,7 +1,7 @@
 const MinesWeeper = require('../lib/MinesWeeper')
 
 createGame = (width, height, bombsNumber) => {
-  return new MinesWeeper(width, height, bombsNumber)
+  return new MinesWeeper(width, height, bombsNumber).stringify()
 }
 
 module.exports = { createGame }
